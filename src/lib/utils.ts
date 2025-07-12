be65872d1c02e5608ff7bbc385617c334532e4cc
cetteup <me@cetteup.com>
2025-07-12 21:09:04 +0200

    feat: add more known servers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -36,6 +36,10 @@ const knownServers: { ip: string, provider: Provider }[] = [
     { ip: '173.234.79.175', provider: 'bf2hub.com' }, // Lost-Soldiers
     { ip: '138.197.130.124', provider: 'bf2hub.com' }, // Weekend Warriors
     { ip: '162.55.244.157', provider: 'bf2hub.com' }, // BF2L (and their customers)
+    { ip: '51.195.137.46', provider: 'bf2hub.com' }, // *BF2 All Maps
+    { ip: '51.68.220.134', provider: 'bf2hub.com' }, // =OLD=
+    { ip: '74.91.114.239', provider: 'bf2hub.com' }, // The Other Guys
+    { ip: '116.62.207.38', provider: 'bf2hub.com' }, // China #1 wuxianzaiju
     { ip: '37.230.210.130', provider: 'playbf2.ru' }, // PlayBF2/T-Gamer
 ];
 
