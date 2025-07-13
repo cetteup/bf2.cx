9c299e0cad010fdc8512b7d8460a7313fe77e764
cetteup <me@cetteup.com>
2025-07-13 09:31:28 +0200

    feat: set minimum cache TTL for images to 12 hours

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        minimumCacheTTL: 12 * 60 * 60
+    }
 };
 
 export default nextConfig;
