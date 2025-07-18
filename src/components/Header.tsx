32d97ef164c53349483ffea1dc2589a919e94f23
cetteup <me@cetteup.com>
2025-07-18 23:59:55 +0200

    fix: move nav links to toggle canvas on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,9 +26,9 @@ export default function Header() {
 
     return (
         <header>
-            <Navbar bg={'dark'} data-bs-theme={'dark'}>
+            <Navbar expand={'lg'} bg={'dark'} data-bs-theme={'dark'}>
                 <Container>
-                    <NavbarBrand as={Link} href={'/'}>
+                    <NavbarBrand as={Link} href={'/'} className={'order-first'}>
                         <Image
                             src={'/bf2.cx.png'}
                             width={24}
@@ -38,16 +38,7 @@ export default function Header() {
                             alt={'BF2.CX'}
                         /> BF2.CX
                     </NavbarBrand>
-                    <NavbarToggle aria-controls={'basic-navbar-nav'}/>
-                    <NavbarCollapse>
-                        <Nav>
-                            <NavLink as={Link} href={'/'} active={path == '/'}>Serverlist</NavLink>
-                            <NavLink as={Link} href={'/faq'} active={path == '/faq'}>FAQ</NavLink>
-                        </Nav>
-                    </NavbarCollapse>
-                    <NavbarText>
-                        <IconLink icon={'bi-discord'} href={'https://discord.gg/fq7c46prEX'} title={'Discord'}/>
-                        <IconLink icon={'bi-github'} href={'https://github.com/cetteup/bf2.cx'} title={'GitHub'}/>
+                    <Nav className={'ms-auto me-2 me-lg-0 order-2 order-lg-1'}>
                         <Button
                             variant={'outline-light'}
                             onClick={() => setShowBuddyList(true)}
@@ -59,7 +50,28 @@ export default function Header() {
                                 <Badge bg={'success'} className={'ms-1 buddy'}>{onlineBuddyCount} online</Badge>
                             }
                         </Button>
-                    </NavbarText>
+                    </Nav>
+                    <NavbarToggle className={'order-3'} aria-controls={'nav-links'}/>
+                    <NavbarCollapse id={'nav-links'} className={'order-last order-lg-first mb-1 mb-lg-0'}>
+                        <Nav>
+                            <NavLink as={Link} href={'/'} active={path == '/'}>Serverlist</NavLink>
+                            <NavLink as={Link} href={'/faq'} active={path == '/faq'}>FAQ</NavLink>
+                            <NavbarText>
+                                <IconLink
+                                    icon={'bi-discord'}
+                                    href={'https://discord.gg/fq7c46prEX'}
+                                    title={'Discord'}
+                                    className={'me-3 mx-lg-3'}
+                                />
+                                <IconLink
+                                    icon={'bi-github'}
+                                    href={'https://github.com/cetteup/bf2.cx'}
+                                    title={'GitHub'}
+                                    className={'me-3 mx-lg-3'}
+                                />
+                            </NavbarText>
+                        </Nav>
+                    </NavbarCollapse>
                 </Container>
             </Navbar>
             <BuddyList
--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -5,12 +5,13 @@ type IconLinkProps = {
     icon: string
     href: string
     title?: string
+    className?: string
 }
 
-export const IconLink: FC<IconLinkProps> = ({ icon, href, title }) => {
+export const IconLink: FC<IconLinkProps> = ({ icon, href, title, className }) => {
     return (
-        <span className="mx-3">
-            <a href={href} title={title} rel="noreferrer">
+        <span className={className ?? 'mx-3'}>
+            <a href={href} title={title} rel={'noreferrer'}>
                 <i className={icon + ' text-white'}/>
             </a>
         </span>
