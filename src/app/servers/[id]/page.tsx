019597d3e1db861c8a787700bb9bb70072037477
cetteup <me@cetteup.com>
2026-02-17 08:43:11 +0100

    feat: add gameppy.com warning to China #1 server detail page

--- src/app/servers/[id]/page.tsx
+++ src/app/servers/[id]/page.tsx
@@ -2,6 +2,7 @@ import { fetchServer } from '@/lib/fetch';
 import { ServerDetail } from '@/components/ServerDetail/ServerDetail';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { Alert, AlertHeading, AlertLink } from 'react-bootstrap';
 
 export const runtime = 'edge';
 
@@ -42,6 +43,22 @@ export default async function ServerPage({ params }: Props) {
 
     return (
         <>
+            {server.name == 'China #1 standalone server' &&
+                <Alert variant={'warning'} dismissible>
+                    <AlertHeading>
+                        <i className={'bi-exclamation-diamond-fill me-2'}/> If you are the admin of this
+                        server and/or the gameppy.com <q>master</q> server,
+                        please note
+                    </AlertHeading>
+                    gameppy.com is using the same player id range as BF2Hub, causing gameppy.com
+                    players to be <i>banned</i> on BF2Hub servers. If possible, please switch to a unique player id
+                    range (e.g. starting with 510000000 instead of 500000000) and migrate existing players.
+                    For details, feel free to reach out via <AlertLink href={'https://discord.gg/fq7c46prEX'}
+                                                               rel={'noreferrer'}>Discord</AlertLink>, <AlertLink
+                    href={'https://hostux.social/@cetteup'} rel={'noreferrer'}>Mastodon</AlertLink> or <AlertLink
+                    href={'mailto:me@cetteup.com'} rel={'noreferrer'}>email</AlertLink>.
+                </Alert>
+            }
             <ServerDetail initial={server}/>
         </>
     );
